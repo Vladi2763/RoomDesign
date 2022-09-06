07c77f12e98bde3689aae7aba33e2a5eb32acad5
Vladislav Teleshov <Necro01@mail.ru>
2022-09-06 10:20:16 +0300

    modified

--- src/App/pages/MainPage/components/Content/Main/ProductsInformation/Products/Products.tsx
+++ src/App/pages/MainPage/components/Content/Main/ProductsInformation/Products/Products.tsx
@@ -1,26 +1,12 @@
-import {
-  switchOn,
-  switchOff,
-} from "../../../../../../../../store/actionsCreater";
-
 import { useSelector } from "react-redux";
 import { InitialState } from "../../../../../../../../store/mainReducer";
 import classes from "./Products.module.scss";
 
 import Product from "../Product/Product";
-import Button from "../ProductMenu/Button/Button";
-import { useDispatch } from "react-redux";
 
 const Products = () => {
   const products = useSelector((state: InitialState) => state.products);
-  const dispatch = useDispatch();
-  const switchOnHandler = () => {
-    dispatch(switchOn());
-  };
 
-  const switchOffHandler = () => {
-    dispatch(switchOff());
-  };
   return (
     <div className={classes.products}>
       {products.map((lamp) => (
